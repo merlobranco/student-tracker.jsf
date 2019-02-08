7de7dda6cdd5f435279563aaa933819b945c2504
Brais Cidras <braisomary@gmail.com>
2019-02-08 18:07:11 +0100

    Adding Application Architecture Core

---
+++ src/com/luv2code/jsf/jdbc/StudentController.java
@@ -0,0 +1,60 @@
+package com.luv2code.jsf.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class StudentController {
+
+	private List<Student> students;
+	private StudentDbUtil studentDbUtil;
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	public StudentController() throws Exception {
+		students = new ArrayList<>();
+		
+		studentDbUtil = StudentDbUtil.getInstance();
+	}
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void loadStudents() {
+
+		logger.info("Loading students");
+		
+		students.clear();
+
+		try {
+			
+			// Get all students from database
+			students = studentDbUtil.getStudents();
+			
+		} catch (Exception exc) {
+			// Send this to server logs
+			logger.log(Level.SEVERE, "Error loading students", exc);
+			
+			// Add error message for JSF page
+			addErrorMessage(exc);
+		}
+	}
+	
+	/**
+	 * Adding a custom error message to the Faces Context
+	 * @param exc
+	 */
+	private void addErrorMessage(Exception exc) {
+		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
+}
